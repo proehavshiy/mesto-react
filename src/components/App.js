5430247b141b6ef35c1d94aa8b5e1667e8ab528b
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-05-05 11:22:20 +0300

    feature: попапы с формами подключены через общий компонент

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import Header from './header/Header'
 import Main from './main/Main'
 import Footer from './footer/Footer'
 import PopupWithForm from './popup_with_form/PopupWithForm'
+import PopupWithImage from './popup_with_image/popupWithImage'
 
 function App() {
   return (
@@ -22,7 +23,60 @@ function App() {
           </section>
           </fieldset>
         </PopupWithForm>
+        <PopupWithForm name="add-card" title="Новое место">
+          <fieldset className="popup__profile-information">
+            <section className="popup__input-section">
+              <input className="popup__input popup__input_location-name" type="text" name="location-name" defaultValue placeholder="Название" required minLength={2} maxLength={30} />
+              <span className="popup__input-error popup__input-error_type_location-name" />
+            </section>
+            <section className="popup__input-section">
+              <input className="popup__input popup__input_image-link" type="url" name="image-link" defaultValue placeholder="Ссылка на картинку" required />
+              <span className="popup__input-error popup__input-error_type_image-link" />
+            </section>
+            </fieldset>
+        </PopupWithForm>
+        <PopupWithForm name="confirm-deletion" title="Вы уверены?">
+        </PopupWithForm>
+        <PopupWithForm name="avatar" title="Обновить аватар">
+          <fieldset className="popup__profile-information">
+            <section className="popup__input-section">
+              <input className="popup__input popup__input_image-link" type="url" name="image-link" defaultValue placeholder="Ссылка на картинку" required />
+              <span className="popup__input-error popup__input-error_type_image-link" />
+            </section>
+          </fieldset>
+        </PopupWithForm>
+        <PopupWithImage />
+      </div>
+    </div>
+  );
+}
 
+export default App;
+
+/*
+<div className="popup popup_change-profile" >
+         <div className="popup__container popup__container_form popup__container_change-profile">
+           <button className="popup__button-close popup__button-close_change-profile page__button" aria-label="Кнопка Закрыть форму" type="button" />
+           <form className="popup__form" action="#" method="POST" name="profile-form" noValidate autoComplete="off">
+             <h2 className="popup__heading">
+               Редактировать профиль
+             </h2>
+             <fieldset className="popup__profile-information">
+               <section className="popup__input-section">
+                 <input className="popup__input popup__input_profile-name" type="text" name="profile-name" defaultValue placeholder="Имя" required minLength={2} maxLength={40} />
+                 <span className="popup__input-error popup__input-error_type_profile-name" />
+               </section>
+               <section className="popup__input-section">
+                 <input className="popup__input popup__input_profile-signing" type="text" name="profile-signing" defaultValue placeholder="Подпись" required minLength={2} maxLength={200} />
+                 <span className="popup__input-error popup__input-error_type_profile-signing" />
+               </section>
+             </fieldset>
+             <button className="popup__button-save popup__button-save_change-profile page__button" type="submit" value="Отправить на сервер" aria-label="Кнопка Сохранить форму">
+               Сохранить
+             </button>
+           </form>
+         </div>
+       </div>
        <div className="popup popup_add-card">
          <div className="popup__container popup__container_form popup__container_add-card">
            <button className="popup__button-close popup__button-close_add-card page__button" aria-label="Кнопка Закрыть форму" type="button" />
@@ -85,37 +139,6 @@ function App() {
              <img className="popup__image" src="https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg" alt="Архыз" />
              <figcaption className="popup__figcaption" />
            </figure>
-         </div>
-       </div>
-      </div>
-    </div>
-  );
-}
-
-export default App;
-
-/*
-<div className="popup popup_change-profile" >
-         <div className="popup__container popup__container_form popup__container_change-profile">
-           <button className="popup__button-close popup__button-close_change-profile page__button" aria-label="Кнопка Закрыть форму" type="button" />
-           <form className="popup__form" action="#" method="POST" name="profile-form" noValidate autoComplete="off">
-             <h2 className="popup__heading">
-               Редактировать профиль
-             </h2>
-             <fieldset className="popup__profile-information">
-               <section className="popup__input-section">
-                 <input className="popup__input popup__input_profile-name" type="text" name="profile-name" defaultValue placeholder="Имя" required minLength={2} maxLength={40} />
-                 <span className="popup__input-error popup__input-error_type_profile-name" />
-               </section>
-               <section className="popup__input-section">
-                 <input className="popup__input popup__input_profile-signing" type="text" name="profile-signing" defaultValue placeholder="Подпись" required minLength={2} maxLength={200} />
-                 <span className="popup__input-error popup__input-error_type_profile-signing" />
-               </section>
-             </fieldset>
-             <button className="popup__button-save popup__button-save_change-profile page__button" type="submit" value="Отправить на сервер" aria-label="Кнопка Сохранить форму">
-               Сохранить
-             </button>
-           </form>
          </div>
        </div>
 */
